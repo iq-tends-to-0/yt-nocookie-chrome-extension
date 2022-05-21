157f60f518007ab70ffdb0d9ecb1fec8ed66354f
Aryan <ahshah26m@gmail.com>
2022-05-21 15:17:47 +0530

    Initial Commit

---
+++ popup.js
@@ -0,0 +1,24 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btn').addEventListener('click', clickHandler);
+})
+
+
+async function getCurrentTab()
+{
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+function clickHandler(e){
+    getCurrentTab().then( tab=>{
+        if ( tab.url.slice(0,23) == "https://www.youtube.com" )
+        {
+            const ytID = tab.url.slice(32,tab.url.length)
+            chrome.tabs.create({url:`https://www.youtube-nocookie.com/embed/${ytID}?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&color=white`});
+            chrome.tabs.remove(tab.id)
+        }
+    });
+}
