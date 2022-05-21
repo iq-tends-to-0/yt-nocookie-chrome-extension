fc271b42dd7b79bac2ee15ffd2346ad8a9a0c702
iq-tends-to-0 <ahshah26m@gmail.com>
2022-05-21 19:22:23 +0530

    Fixed the videos from playlist

--- popup.js
+++ popup.js
@@ -16,9 +16,11 @@ function clickHandler(e){
     getCurrentTab().then( tab=>{
         if ( tab.url.slice(0,23) == "https://www.youtube.com" )
         {
-            const ytID = tab.url.slice(32,tab.url.length)
+            const ytID = tab.url.slice(32,(tab.url.indexOf("&")))
             chrome.tabs.create({url:`https://www.youtube-nocookie.com/embed/${ytID}?modestbranding=1&showinfo=0&rel=0&iv_load_policy=3&color=white`});
             chrome.tabs.remove(tab.id)
         }
     });
 }
+
+const a = "foo"
